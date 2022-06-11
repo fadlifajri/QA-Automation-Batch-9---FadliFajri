a926e36266ff769136e84336e8914f72772c1ef0
fadlifajri <fadlifajri@gmail.com>
2022-06-11 22:42:09 +0700

    added tugas 5 & 7

--- src/test/java/tugas05/TugasAPITest2.java
+++ src/test/java/tugas05/TugasAPITest2.java
@@ -1,40 +1,45 @@
 package tugas05;
 
-import org.json.JSONObject;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 import com.github.javafaker.Faker;
 
+import io.restassured.http.ContentType;
 
-public class TugasAPITest {
+
+public class TugasAPITest2 {
 	
 
 
 	@Test
 	//Create user successfully
 	public void createUser1() {
+		
 		Faker faker = new Faker();
+		
 		
 		String username = faker.name().fullName().replaceAll(" ", "");
 		String password = faker.internet().password();
 		String email = faker.internet().emailAddress();
 		String phonenumber = faker.phoneNumber().cellPhone().trim();
 		
-		JSONObject body = new JSONObject();
-	
+		Map<String, String> body = new HashMap<String, String>();
 		body.put("username", username);
 		body.put("password", password);
 		body.put("email", email);
 		body.put("phonenumber", phonenumber);
 	
-		System.out.println(body);
+
 		given().
-			body(body).
+			baseUri("https://kolakproject.herokuapp.com/register").contentType(ContentType.JSON).body(body).log().all().
 		when().
-			post("https://kolakproject.herokuapp.com/register").
+			post().
 		then().
-			statusCode(201).log().all();
+			assertThat().statusCode(201);
 		
 		
 	}
